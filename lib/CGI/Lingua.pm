44ab1d89bf6aab49ea0e7a3a7c6603519ed0a1a6
Nigel Horne <njh@bandsman.co.uk>
2015-01-23 17:37:50 -0500

    Added comment about Geo::IPfree

--- lib/CGI/Lingua.pm
+++ lib/CGI/Lingua.pm
@@ -1050,7 +1050,7 @@ L<http://search.cpan.org/dist/CGI-Lingua/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2010-2014 Nigel Horne.
+Copyright 2010-2015 Nigel Horne.
 
 This program is released under the following licence: GPL
 
