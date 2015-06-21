9e41682bfecebecfe5de342364321b1f91fd31ee
Nigel Horne <njh@bandsman.co.uk>
2015-06-21 08:45:18 -0400

    Release 0.53

--- lib/CGI/Lingua.pm
+++ lib/CGI/Lingua.pm
@@ -5,7 +5,7 @@ use strict;
 use Class::Autouse qw{Carp Locale::Language Locale::Object::Country Locale::Object::DB I18N::AcceptLanguage I18N::LangTags::Detect Storable};
 
 use vars qw($VERSION);
-our $VERSION = '0.52';
+our $VERSION = '0.53';
 
 =head1 NAME
 
@@ -13,7 +13,7 @@ CGI::Lingua - Create a multilingual web page
 
 =head1 VERSION
 
-Version 0.52
+Version 0.53
 
 =cut
 
