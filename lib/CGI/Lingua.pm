f919798dd7fceb4dfc24236e8fa31a68e7878ef1
Nigel Horne <njh@bandsman.co.uk>
2025-09-03 15:31:00 -0400

    Added t/priority.t

--- lib/CGI/Lingua.pm
+++ lib/CGI/Lingua.pm
@@ -646,9 +646,9 @@ sub _find_language
 				}
 			}
 		} elsif($http_accept_language =~ /;/) {
-			# e.g. HTTP_ACCEPT_LANGUAGE=de-DE,de;q=0.9
-			# and we don't support DE at all
-			$self->_notice(__PACKAGE__, ': ', __LINE__, ": lower priority supported language may be missed HTTP_ACCEPT_LANGUAGE=$http_accept_language");
+			# e.g. HTTP_ACCEPT_LANGUAGE=de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7
+			# and we don't support DE at all, but we do accept en-US
+			$self->_notice(__PACKAGE__, ': ', __LINE__, ": lower priority supported language may be missed HTTP_ACCEPT_LANGUAGE=$http_accept_language, supported languages are: ", join(',', @{$self->{supported}}));
 		}
 		if($self->{_slanguage} && ($self->{_slanguage} ne 'Unknown')) {
 			if($self->{_rlanguage} eq 'Unknown') {
